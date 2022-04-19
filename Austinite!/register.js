89cbab55a2668a7c3e72f3098f8aab92822e14ec
youadin <42190043+youadin@users.noreply.github.com>
2022-04-18 20:32:20 -0500

    Phase5
    This is my PHP updated version - Yousuf

--- Austinite!/register.js
+++ Austinite!/register.js
@@ -1,48 +1,19 @@
-function register(){
-	var name = document.getElementById("name").value
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
-	var vpassword = document.getElementById("vpassword").value;
-	var letterNumber = /^[0-9a-zA-Z]+$/;
-	var num = /^[0-9]+$/;
-	var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,10}$/;
+document.getElementById("register").onclick = verification;
 
-	if (name == ""){
-		window.alert("Please enter your name")
+function verification() {
+	var registration = document.getElementById("registration");
+	var username = registration.user.value;
+	var password = registration.psw.value;
+	var password_repeat = registration.psw2.value;
+
+	if (username.length > 10 || username.length < 6 || !/^[A-Za-z0-9]*$/.test(username) || !isNaN(username.charAt(0))) {
+		alert("Invalid username or password")
 	}
-
-	if (username == ""){
-		window.alert("Please enter usename")
+	else if (password != password_repeat || password.length > 10 || password.length < 6 || !/^[A-Za-z0-9]*$/.test(password) || (!/\d/.test(password) || !/[a-z]/.test(password) || !/[A-Z]/.test(password))) {
+		alert("Invalid username or password")
 	}
+	else {
+		alert("User validated")
 
-	else if ((username.length < 6) || (username.length > 10)){
-		window.alert("Invalid username. Try again")
 	}
-
-	else if (!username.match(letterNumber)){
-		window.alert("Invalid username. Try again")
-	}
-
-	else if (username.charAt(0).match(num)){
-		window.alert("Invalid username. Try again")
-	}
-
-	if (password == ""){
-		window.alert("Please enter password")
-	}
-
-	else if ((password.length < 6) || (password.length > 10)){
-		window.alert("Invalid password. Try again")
-	}
-
-	else if (!password.match(passw)){
-		window.alert("Invalid password. Try again")
-	}
-
-	if (vpassword !== password){
-		window.alert("Password verification failed. Try again")
-	}
-
-	
-
 }
